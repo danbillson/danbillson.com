1fad99216f5399fe124dd5ffae9a36fad9c79171
Dan Billson <dbillson@outlook.com>
2024-06-15 23:23:25 +0100

    📝 blog: write first blog post

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,15 @@
+import { allPosts } from "contentlayer/generated";
+
 export default async function sitemap() {
   let routes = ["", "/work", "/projects", "/blog"].map((route) => ({
     url: `https://danbillson.com${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return routes;
+  let blogs = allPosts.map((post) => ({
+    url: `https://danbillson.com${post.slug}`,
+    lastModified: new Date(post.date).toISOString().split("T")[0],
+  }));
+
+  return [...routes, ...blogs];
 }
