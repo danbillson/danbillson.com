a95b84dbe65082487883108ffad75a7dbacb7864
Dan Billson <dbillson@outlook.com>
2025-07-05 23:37:23 +0100

    feat: work sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Karla as FontSans } from "next/font/google";
+import { Work_Sans as FontSans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: {
